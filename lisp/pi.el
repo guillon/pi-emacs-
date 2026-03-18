bb3806b294be28ac65ee3cf27641ad2ff813bcb4
Christophe Guillon <christophe.guillon@inria.fr>
2026-03-18 15:02:00 +0100

    Add package metadata headers
    Co-Authored-By: "gpt5.4 medium" <agent@noreply.inria.fr>

--- lisp/pi.el
+++ lisp/pi.el
@@ -1,4 +1,7 @@
 ;;; pi.el --- Unified Emacs frontend for pi -*- lexical-binding: t; -*-
+;; Version: 0.1.0
+;; Package-Requires: ((emacs "29.1"))
+;; Keywords: tools, convenience
 ;; SPDX-License-Identifier: MIT
 ;; Copyright (c) 2026 The pi-emacs Project Authors
 
